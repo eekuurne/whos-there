991c3614a37704b98467fa7197f293f9c77444e8
maimsy <jhilum@hotmail.com>
2019-01-26 17:00:01 +0200

    updated menu and added credit scene

---
+++ WhosThere/Assets/Scripts/UIManagerScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManagerScript : MonoBehaviour
+{
+
+    private void Start()
+    {
+        
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Menu();
+        }
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+
+    public void Credits() {
+        SceneManager.LoadScene("Credits");
+    }
+}
