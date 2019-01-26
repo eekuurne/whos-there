c9ffbee06bbf2330cb5df2536864ee6cbf35235a
Eero Kuurne <eero.kuurne@hotmail.com>
2019-01-26 13:22:31 +0200

    Player, GUI, hand

---
+++ WhosThere/Assets/Scripts/Hand.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour {
+
+    [SerializeField] GameObject gun;
+
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+}
